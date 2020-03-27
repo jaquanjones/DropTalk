d1c70535a817f32b57e8e30674061a57e70eb8cf
Jaquan Jones <jayj280@gmail.com>
2020-03-26 18:15:20 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:chatapp/screens/welcome_screen.dart';
+import 'package:chatapp/screens/login_screen.dart';
+import 'package:chatapp/screens/registration_screen.dart';
+import 'package:chatapp/screens/chat_screen.dart';
+
+void main() => runApp(ChatApp());
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
